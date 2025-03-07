89cfaa004c3eb1883b357ffc742b62e17d7ca688
Ana Costa Eduardo <ana.costa.eduardo@gmail.com>
2025-03-06 22:06:50 -0300

    feat: filter tasks by status

--- src/app/shared/list/list.component.ts
+++ src/app/shared/list/list.component.ts
@@ -6,22 +6,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./list.component.css'],
 })
 export class ListComponent {
-  filteredTasks: any;
-  filter: any;
+  filteredTasks: { task: string; status: string }[] = [];
+  filter: string = 'all';
   tasks: { task: string; status: string }[] = [];
 
   addTask(newTask: { task: string; status: string }) {
     this.tasks.push(newTask);
+    this.applyFilter();
     console.log('Tasks:', this.tasks);
   }
 
   updateTask(updatedTask: { task: string; status: string }) {
-    this.tasks = this.tasks
-      .map((task) => (task.task === updatedTask.task ? updatedTask : task))
-      .sort((a, b) => (a.status === 'incomplete' ? -1 : 1));
+    this.tasks = this.tasks.map((task) =>
+      task.task === updatedTask.task ? updatedTask : task
+    );
+    this.applyFilter();
   }
 
   deleteTask(taskToDelete: { task: string; status: string }) {
     this.tasks = this.tasks.filter((task) => task.task !== taskToDelete.task);
+    this.applyFilter();
+  }
+
+  setFilter(filter: string) {
+    this.filter = filter;
+    this.applyFilter();
+  }
+
+  applyFilter() {
+    if (this.filter === 'all') {
+      this.filteredTasks = this.tasks;
+    } else {
+      this.filteredTasks = this.tasks.filter(
+        (task) => task.status === this.filter
+      );
+    }
   }
 }
