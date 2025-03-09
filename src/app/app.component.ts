6ed71aeb9b22dcd2a333fc2d6fef24a528ab9b23
Ana Costa Eduardo <ana.costa.eduardo@gmail.com>
2025-03-09 04:13:05 -0300

    feature: recruiter message service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import { RecruiterMessageService } from './shared/services/recruiter-message/recruiter-message.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor(private recruiterMessage: RecruiterMessageService) {}
+
+  ngOnInit(): void {
+    this.recruiterMessage.showRecruiterMessage();
+  }
+}
--- src/app/shared/services/recruiter-message/recruiter-message.service.spec.ts
+++ src/app/shared/services/recruiter-message/recruiter-message.service.spec.ts
@@ -1,5 +1,4 @@
 import { TestBed } from '@angular/core/testing';
-
 import { RecruiterMessageService } from './recruiter-message.service';
 
 describe('RecruiterMessageService', () => {
@@ -12,5 +11,13 @@ describe('RecruiterMessageService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should show recruiter message', () => {
+    spyOn(console, 'clear');
+    spyOn(console, 'log');
+    service.showRecruiterMessage();
+    expect(console.clear).toHaveBeenCalled();
+    expect(console.log).toHaveBeenCalled();
   });
 });
